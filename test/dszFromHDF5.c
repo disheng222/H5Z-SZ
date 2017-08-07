eeef8b4bb514468f90b405ab8b648358d6a221ec
Sheng Di <sdi1@anl.gov>
2017-08-07 13:11:47 -0500

    revise dszFromHDF5.c such that it doesn't need -f in the decompression.

--- test/dszFromHDF5.c
+++ test/dszFromHDF5.c
@@ -20,12 +20,13 @@
 
 int main(int argc, char * argv[])
 {
-	int dimSize = 0, dataType = 0;
+	int dimSize = 0;
 	size_t r5=0,r4=0,r3=0,r2=0,r1=0,nbEle = 0;
 	char hdf5FilePath[640], outputFilePath[640];
-	hid_t file, dset, dcpl, space_id; /*Handles*/
+	hid_t file, dset, dcpl, space_id, dtype; /*Handles*/
 	H5Z_filter_t filter_id = 0;
 	herr_t status;
+	H5T_class_t type_class;
 
 	htri_t avail;
 	char filter_name[80];
@@ -34,15 +35,14 @@ int main(int argc, char * argv[])
 	unsigned int values_out[7] = {0,0,0,0,0,0,0}; //at most 7 parameters 
 	float* data = NULL;
 
-	if(argc < 3)
+	if(argc < 2)
 	{
-		printf("Test case: dszFromHDF5 [datatype] [hdf5FilePath]\n");
-		printf("Example: szToHDF5 -f/-d testdata/x86/testfloat_8_8_128.dat.sz.hdf5\n");
+		printf("Test case: dszFromHDF5 [hdf5FilePath]\n");
+		printf("Example: szToHDF5 testdata/x86/testfloat_8_8_128.dat.sz.hdf5\n");
 		exit(0);
 	}
 
-	dataType = strcmp(argv[1],"-f")==0 ? 0: 1;
-	sprintf(hdf5FilePath, "%s", argv[2]);
+	sprintf(hdf5FilePath, "%s", argv[1]);
 	sprintf(outputFilePath, "%s.out.h5", hdf5FilePath);
 
 	/*Open the hdf5 file with SZ-compressed data*/
@@ -51,25 +51,6 @@ int main(int argc, char * argv[])
     
     /*Retrieve dataset creation property list.*/
     dcpl = H5Dget_create_plist(dset);
-    
-    /*Retrieve and print the filter id, compression level and filter's name for SZ*/
-    /*filter_id = H5Pget_filter2(dcpl, (unsigned)0, &flags, &nelmts, values_out, sizeof(filter_name), filter_name, NULL);
-    printf("values_out[0]=%u, values_out[1]=%u\n", values_out[0], values_out[1]);
-    printf(" Filter identifier is: %s, id = %d\n", filter_name, filter_id);
-    
-    switch(filter_id)
-    {
-	case H5Z_FILTER_SZ:
-		SZ_cdArrayToMetaData(nelmts, values_out, &dimSize, &dataType, &r5, &r4, &r3, &r2, &r1);
-		if(dataType==SZ_FLOAT)
-			printf("data type: FLOAT\n");
-		else
-			printf("data type: DOUBLE\n");
-		break;
-	default:
-		printf("Not expected filter\n");
-		exit(0);
-	}*/
 	
     /*Check that filter is not registered with the library yet*/
 	avail = H5Zfilter_avail(H5Z_FILTER_SZ);
@@ -81,17 +62,36 @@ int main(int argc, char * argv[])
 	space_id = H5Dget_space(dset);	
 	nbEle = H5Sget_simple_extent_npoints(space_id);
 	data = (float*)malloc(sizeof(float)*nbEle);	
+	
+	if((dtype = H5Dget_type(dset)) < 0)
+		printf("Error: H5Dget_type(dset) < 0\n");
+
 	/*Read the data using the default properties.*/
 	printf("....Reading SZ compressed data .....................\n");
+
+	if((type_class = H5Tget_class(dtype)) < 0)
+		printf("Error: H5Tget_class<0\n");
+
+	switch (type_class)
+	{
+	case H5T_FLOAT:
+		if (H5Tequal(dtype, H5T_IEEE_F32BE) == 1 || H5Tequal(dtype, H5T_IEEE_F32LE) == 1
+		|| H5Tequal(dtype, H5T_NATIVE_FLOAT) == 1) 
+		{
+			printf("data type: float\n");
+			status = H5Dread(dset, H5T_IEEE_F32LE, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+        }
+		else //64bit: double 
+		{
+			printf("data type: double\n");
+			status = H5Dread(dset, H5T_IEEE_F64LE, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+		}
+		break;
+	default: 
+		printf("Error: support only float and double currently\n");
+		exit(0);
+	}
 	
-	if(dataType==SZ_FLOAT)
-	{
-		status = H5Dread(dset, H5T_IEEE_F32LE, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
-	}
-	else
-	{
-		status = H5Dread(dset, H5T_IEEE_F64LE, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
-	}
 	/*Print the first 10 data values to check the correctness.*/	
 	int i;
 	printf("reconstructed data = ");
--- test/test_decompress.sh
+++ test/test_decompress.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo dszFromHDF5 -f testdata/x86/testfloat_8_8_128.dat.sz.h5
-dszFromHDF5 -f testdata/x86/testfloat_8_8_128.dat.sz.h5
+dszFromHDF5 testdata/x86/testfloat_8_8_128.dat.sz.h5
 
